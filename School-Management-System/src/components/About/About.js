0908b79d55bd878678da8e76224bdbbf61203dd6
vivek0913 <83529785+vivek0913@users.noreply.github.com>
2022-11-24 21:09:53 +0530

    Add files via upload

---
+++ School-Management-System/src/components/About/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './About.scss';
+function About() {
+	return (
+		<div id='About'>
+			<h1 style={{ textAlign: 'center' }}>About The Application</h1>
+			<div className='row'>
+				
+				<div className='col-md-6'>
+					<p>
+						This is a Student Management System in which we can manage the school subjects, teachers,
+						students and classes.
+
+						We can add Student's details, Subject details, Teacher's details and Class details as well.
+					</p>
+					
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default About;
